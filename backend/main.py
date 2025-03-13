de4584592a51c8d8741b1d8014699bf8e1af2715
poltergeistjoya <joyaranidebi@gmail.com>
2025-03-13 18:04:58 -0400

    Map appears but does not display polygons

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from models.locations import Base
+from fastapi import FastAPI, Depends
+from sqlalchemy import create_engine, select
+from sqlalchemy.orm import sessionmaker, Session
+from models.locations import Base, Location
+from geoalchemy2.functions import ST_AsGeoJSON
 
 DATABASE_URL = "postgresql://postgres:password@localhost:5432/urban_utilization"
 
@@ -14,9 +15,34 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind = engine)
 #Create tables if they don't exist 
 Base.metadata.create_all(bind=engine)
 
+session = Session(bind=engine)
+
+# Dependancy to get a database session 
+# TODO Make dependancies.py
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 @app.get("/")
 def read_root():
     return {"message":"FastAPI + PostGIS API is running!"}
+
+#TODO make this into a {} so specific locs can be panned to 
+@app.get("/locations/")
+def get_locations(db: Session = Depends(get_db)):
+    locations = db.scalars(select(Location)).all()
+
+    return [
+        {
+            "id": loc.id,
+            "name": loc.name, 
+            "geometry": db.scalar(ST_AsGeoJSON(loc.geom)),
+        }
+        for loc in locations
+    ]
 
 if __name__ == "__main__":
     import uvicorn
